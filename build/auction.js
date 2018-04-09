97c97f667dda62e724d5e38245487fd66a7bc80a
castriganoj <castriganoj@gmail.com>
2018-04-09 16:32:56 -0400

    Update static paths after rearranging server and client directories.

--- build/auction.js
+++ build/auction.js
@@ -5,8 +5,8 @@ var path = require("path");
 var model_1 = require("./model");
 // HTTP API
 var app = express();
-app.use('/node_modules', express.static(path.join('../client/node_modules')));
-app.use('/', express.static(path.join('../client/dist')));
+app.use('/node_modules', express.static(path.join('./client/node_modules')));
+app.use('/', express.static(path.join('./client/dist')));
 app.get('/products', function (req, res) {
     res.json(model_1.getProducts(req.query));
 });
@@ -16,7 +16,11 @@ app.get('/products/:id', function (req, res) {
 app.get('/products/:id/reviews', function (req, res) {
     res.json(model_1.getReviewsByProductId(parseInt(req.params.id)));
 });
-var httpServer = app.listen(8000, 'localhost', function () {
+// const httpServer: HttpServer = app.listen(8000, 'localhost', () => {
+//   const {address, port} = httpServer.address();
+//   console.log('Listening on %s %s', address, port);
+// });
+var httpServer = app.listen(8000, function () {
     var _a = httpServer.address(), address = _a.address, port = _a.port;
     console.log('Listening on %s %s', address, port);
 });
