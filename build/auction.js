197a82df610025da1719acff1ed2e4c239c60222
castriganoj <castriganoj@gmail.com>
2018-04-11 12:20:15 -0400

    Did not compile typescript form previos commit.

--- build/auction.js
+++ build/auction.js
@@ -3,10 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var path = require("path");
 var model_1 = require("./model");
-// HTTP API
 var app = express();
-app.use('/node_modules', express.static(path.join('../client/node_modules')));
-app.use('/', express.static(path.join('../client/dist')));
+app.use('/node_modules', express.static(path.join('./client/node_modules')));
+app.use('/', express.static(path.join('./client/dist')));
 app.get('/products', function (req, res) {
     res.json(model_1.getProducts(req.query));
 });
@@ -20,7 +19,20 @@ app.get('/products/:id/reviews', function (req, res) {
 //   const {address, port} = httpServer.address();
 //   console.log('Listening on %s %s', address, port);
 // });
-var httpServer = app.listen(8000, function () {
+var port = normalizePort(process.env.PORT || '3000');
+var httpServer = app.listen(port, function () {
     var _a = httpServer.address(), address = _a.address, port = _a.port;
     console.log('Listening on %s %s', address, port);
 });
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+    return false;
+}
